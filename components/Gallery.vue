482b2fdb05f236e6e3a764c5d23f62017c3a0559
Michael Rupert <rupertdev@outlook.com>
2019-10-22 15:17:25 -0400

    adding lazy image loading

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,10 +1,12 @@
 <template>
     <ul>
-        <img @click="viewImage(image)" class="gallery-image" v-for="(image, index) in imageLinks" :key=index :src=image>
+	    <v-lazy-image @click="viewImage(image)" class="gallery-image" v-for="(image, index) in imageLinks" :key=index :src=image :src-placeholder=getLowResImage(image)>
     </ul>
 </template>
 
 <script>
+import VLazyImage from "v-lazy-image";
+
 export default {
     data() {
         return {
@@ -19,8 +21,10 @@ export default {
         imageLinks: function () {
             var links = [];
             for (var i = 0; i < this.galleryData.length; i++) {
-                console.log(this.galleryData[i])
-                links.push('/galleries/' + this.galleryName + '/' + this.galleryData[i])
+                if (!this.galleryData[i].includes('lowres')) {
+                    console.log(this.galleryData[i])
+                    links.push('/galleries/' + this.galleryName + '/' + this.galleryData[i])
+                }
             }
             return links
         }
@@ -28,15 +32,29 @@ export default {
     methods: {
         viewImage: function(imagePath) {
             window.open(imagePath)
+        },
+        getLowResImage: function(imagePath) {
+            return imagePath + "-lowres.jpg"
         }
+    },
+    components: {
+    	VLazyImage
     }
-
 }
 </script>
 
 <style lang="scss" scoped>
 .gallery-image {
     max-height: 512px;
+    min-height: 256px;
     margin: 10px;
 }
+.v-lazy-image {
+   filter: blur(10px);
+   transition: filter 0.5s;
+}
+.v-lazy-image-loaded {
+   filter: blur(0);
+}
+
 </style>
